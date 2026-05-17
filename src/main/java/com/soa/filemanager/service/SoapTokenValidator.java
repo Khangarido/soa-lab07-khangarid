cbddab27046773ca4ed26cb9af5e4168b130ccce
Khangarido <khangariderdenechuluun@gmail.com>
2026-05-18 01:18:21 +0800

    Fix typo in validateToken method name and add request builder

--- src/main/java/com/soa/filemanager/service/SoapTokenValidator.java
+++ src/main/java/com/soa/filemanager/service/SoapTokenValidator.java
@@ -1,6 +1,5 @@
 package com.soa.filemanager.service;
 
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,22 +7,16 @@ import org.springframework.http.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 @Service
 public class SoapTokenValidator {
-
-
     private static final Logger log = LoggerFactory.getLogger(SoapTokenValidator.class);
-
 
     @Value("${soap.service.url}")
     private String soapServiceUrl;
 
-
     private final RestTemplate restTemplate = new RestTemplate();
 
-
-    public boolean validahteToken(String token) {
+    public boolean validateToken(String token) {
         try {
             String soapRequest = buildValidateTokenRequest(token);
             HttpHeaders headers = new HttpHeaders();
@@ -40,3 +33,10 @@ public class SoapTokenValidator {
         }
     }
 
+    private String buildValidateTokenRequest(String token) {
+        return String.format(
+            "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:auth=\"http://example.com/auth\">" +
+            "<soapenv:Body><auth:validateTokenRequest><auth:token>%s</auth:token></auth:validateTokenRequest></soapenv:Body>" +
+            "</soapenv:Envelope>", token);
+    }
+}
