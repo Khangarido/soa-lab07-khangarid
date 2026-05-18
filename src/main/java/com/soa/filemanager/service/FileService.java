99dc3cd54228171ef66bc4694a22518c09364d6f
Khangarido <khangariderdenechuluun@gmail.com>
2026-05-18 08:37:26 +0800

    Refactor uploadFile method for improved logging

--- src/main/java/com/soa/filemanager/service/FileService.java
+++ src/main/java/com/soa/filemanager/service/FileService.java
@@ -1,7 +1,6 @@
 package com.soa.filemanager.service;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.soa.filemanager.model.UploadResponse;
@@ -14,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-// DO Spaces руу файл хуулах үндсэн сервис
 @Service
 public class FileService {
 
@@ -23,44 +21,37 @@ public class FileService {
     @Autowired
     private AmazonS3 spacesClient;
 
-    // environment variable-аас bucket нэрийг авна
     @Value("${spaces.bucket-name}")
     private String bucketName;
 
     @Value("${spaces.region}")
     private String region;
 
-    // Файлыг DO Spaces руу хуулах
-    // Буцаах утга: UploadResponse (URL, fileName, success)
     public UploadResponse uploadFile(MultipartFile file) throws IOException {
-        // Файлын нэрийг timestamp-тай болгож давхцахаас сэргийлнэ
         String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
 
-        log.info("Файл хуулж байна: {}", fileName);
+        log.info("Uploading file: {}", fileName);
 
-        // Файлын мэтадата тохируулна
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(file.getSize());
         metadata.setContentType(file.getContentType());
 
-        // DO Spaces руу хуулж, нийтэд унших эрх өгнө (PublicRead)
         PutObjectRequest request = new PutObjectRequest(
-                bucketName,
-                fileName,
-                file.getInputStream(),
-                metadata
-        ).withCannedAcl(CannedAccessControlList.PublicRead);
+            bucketName,
+            fileName,
+            file.getInputStream(),
+            metadata
+        );
 
         spacesClient.putObject(request);
 
-        // Нийтэд харагдах URL бүтээнэ
         String publicUrl = String.format(
-                "https://%s.%s.digitaloceanspaces.com/%s",
-                bucketName, region, fileName
+            "https://%s.%s.digitaloceanspaces.com/%s",
+            bucketName, region, fileName
         );
 
-        log.info("Файл амжилттай хуулагдлаа: {}", publicUrl);
+        log.info("File uploaded successfully: {}", publicUrl);
 
-        return new UploadResponse(publicUrl, fileName, true, "Амжилттай хуулагдлаа");
+        return new UploadResponse(publicUrl, fileName, true, "Upload successful");
     }
-}
+            }
